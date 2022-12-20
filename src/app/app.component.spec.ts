eb8481daef5967baba65307f4d83aa854c4bb643
Fernanda Renata Zaccaro Rigolin <frzrigolin@gmail.com>
2022-12-20 08:56:37 -0300

    Config Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testeAngular'`, () => {
+  it(`should have as title 'conFusion'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testeAngular');
+    expect(app.title).toEqual('conFusion');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testeAngular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('conFusion app is running!');
   });
 });
