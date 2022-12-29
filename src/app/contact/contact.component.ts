431609c515e560ad339e6bf87cadfcd4627518a8
Fernanda Renata Zaccaro Rigolin <frzrigolin@gmail.com>
2022-12-29 11:21:50 -0300

    Reactive Forms Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {NgForm} from '@angular/forms';
 
 import { Feedback, ContactType } from '../shared/feedback';
 
@@ -10,30 +11,43 @@ import { Feedback, ContactType } from '../shared/feedback';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  
   feedbackForm: FormGroup | any;
   feedback: Feedback | any;
   contactType = ContactType;
-
+  @ViewChild('fform') feedbackFormDirective: NgForm | any;
+  
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
   
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required ],
+      lastname: ['', Validators.required ],
+      telnum: ['', Validators.required ],
+      email: ['', Validators.required ],
       agree: false,
       contacttype: 'None',
       message: ''
     });
   }
 
+
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    this.feedbackFormDirective.resetForm();
   }
+
 
 }
